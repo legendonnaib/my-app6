9d9f564933051fc7de77c2adb331e43c0be7180f
legendonnaib <160251630+legendonnaib@users.noreply.github.com>
2024-12-01 18:32:46 +0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,12 @@
+"use client";
+
+import Image from "next/image";
+
 export default function Header() {
   return (
     <header className="bg-white shadow-md">
-     
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
-     
+        {/* Logo Section */}
         <div className="flex items-center space-x-4">
           <Image
             src={"/images/Dsngr.png"}
@@ -15,7 +18,7 @@ export default function Header() {
           <h1 className="text-lg font-bold">Dodgxr</h1>
         </div>
 
- 
+        {/* Navigation Links */}
         <nav className="hidden sm:block">
           <ul className="flex space-x-6">
             <li>
@@ -41,6 +44,7 @@ export default function Header() {
           </ul>
         </nav>
 
+        {/* Mobile Menu Button */}
         <div className="block sm:hidden">
           <button className="text-blue-500">
             <svg
@@ -60,9 +64,6 @@ export default function Header() {
           </button>
         </div>
       </div>
-    
-      {/*justify-right absolute w-1/3 pt-[64px] pr-[24px] pl-[24px] pb-[64px]*/}
-
     </header>
   );
 }
