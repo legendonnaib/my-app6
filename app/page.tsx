0f64de757f3709fc51c1842512d1bd4c960eb178
legendonnaib <160251630+legendonnaib@users.noreply.github.com>
2024-12-01 18:00:59 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,19 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import Categories from "./components/Categories";
 import Achievements from "./components/Achievements";
 import Courses from "./components/Courses";
 import Footer from "./components/Footer";
 
-
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <HeroSection/>
+      <Header />
+      <HeroSection />
       <Categories />
-      <Achievements/>
-      <Courses/>
-      <Footer/>
-      
-     
+      <Achievements />
+      <Courses />
+      <Footer />
     </div>
-    
-    
   );
 }
